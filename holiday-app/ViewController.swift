8710f7af80856da939f2c4ae9003d030bdcbafb9
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-10 16:58:50 +0200

    IBOutlets and IBAction connected

--- holiday-app/ViewController.swift
+++ holiday-app/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var darkView: UIView!
+    @IBOutlet weak var getStartedButton: UIButton!
+    
+    @IBAction func getStartedButtonTapped(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
