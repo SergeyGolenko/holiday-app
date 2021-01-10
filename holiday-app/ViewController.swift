2cf77713bef92c425111ffbb63cf4b2719cba562
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-10 17:30:38 +0200

    lifecycle methods

--- holiday-app/ViewController.swift
+++ holiday-app/ViewController.swift
@@ -9,17 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    //MARK: - IBOutlets and IBAction
     @IBOutlet weak var darkView: UIView!
     @IBOutlet weak var getStartedButton: UIButton!
-    
     @IBAction func getStartedButtonTapped(_ sender: Any) {
     }
     
+    //MARK: - Navigation controller methods
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    //MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
     }
 
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated:animated)
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated:animated)
+    }
+    
+    //MARK: - functions
+    private func setupViews(){
+        getStartedButton.layer.cornerRadius = 28
+        getStartedButton.layer.masksToBounds = true
+        
+    }
 
 }
 
